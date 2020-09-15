aa96479f97be5e2c8f6372fbd53746ca348ac106
Lauraedo Ortega <46540562+Laureado@users.noreply.github.com>
2020-09-14 20:50:09 -0400

    added enemies

---
+++ Assets/EnemyScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    Main Gm;
+    public float BackForce;
+    bool isDead;
+    void Start()
+    {
+        Gm = GameObject.FindGameObjectWithTag("GameController").GetComponent<Main>();
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && !isDead)
+        {
+            Debug.Log("Si");
+            Gm.GameOver();
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Feet"))
+        {
+            isDead = true;
+            Debug.Log("No");
+            collision.gameObject.GetComponentInParent<Rigidbody2D>().velocity = Vector2.up * BackForce;
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/EnemyWeakScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWeakScript : MonoBehaviour
+{
+    public GameObject Enemy;
+    public float BackForce;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            Debug.Log("no");
+            Destroy(Enemy);
+            collision.GetComponent<Rigidbody2D>().velocity = Vector2.up * BackForce;
+        }
+    }
+}
